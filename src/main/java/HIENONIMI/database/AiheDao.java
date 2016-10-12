3de1103df58889651952b94e029b31bf5a9d75a8
hanninev <ville.hanninen94@gmail.com>
2016-10-12 23:33:56 +0300

    Korjaus

--- src/main/java/HIENONIMI/database/AiheDao.java
+++ src/main/java/HIENONIMI/database/AiheDao.java
@@ -6,19 +6,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class AiheDao {
+
     private Database database;
+
     /*
     En implementannu Dao tähän, koska tätä käytetään vain yhdessä haussa ja Dao:n
     kaikki metodit ovat turhia. Ehkä..
-    */
+     */
 
     public AiheDao(Database database) {
         this.database = database;
     }
-    
+
     public List<Aihe> findAll(int alueId) throws SQLException {
         // tähänkin timestamp
-        String komento = "SELECT Alue.id, Aihe.Alue_id, Aihe.nimi, count(Viesti.id) AS viesteja FROM Alue JOIN Aihe ON Alue.id = Aihe.Alue_id JOIN Viesti ON Aihe.id = Viesti.Aihe_id WHERE Aihe.alue_id = ? GROUP BY Aihe.id";
+        String komento = "SELECT Aihe.id, Aihe.Alue_id, Aihe.nimi, count(Viesti.id) AS viesteja FROM Alue JOIN Aihe ON Alue.id = Aihe.Alue_id JOIN Viesti ON Aihe.id = Viesti.Aihe_id WHERE Aihe.alue_id = ? GROUP BY Aihe.id";
         List<Aihe> aiheet = database.queryAndCollect(komento, new AiheCollector(), alueId);
 
         if (aiheet.isEmpty()) {
