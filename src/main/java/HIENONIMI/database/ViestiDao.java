ef2c0e89f269d52ca1f6097c76430380bcbbdef2
hanninev <ville.hanninen94@gmail.com>
2016-10-12 19:30:42 +0300

    Lisätty teeUusi-metodi

--- src/main/java/HIENONIMI/database/ViestiDao.java
+++ src/main/java/HIENONIMI/database/ViestiDao.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ViestiDao {
+
     private Database database;
 
     public ViestiDao(Database database) {
         this.database = database;
     }
-    
+
     public List<Viesti> findAll(int aiheId) throws SQLException {
         String komento = "SELECT Viesti.id as id, Viesti.aihe_id as aihe_id, Viesti.kayttaja_id as kayttaja_id, viesti.viesti, viesti.aika, Kayttaja.nimi as kayttaja FROM Viesti INNER JOIN Kayttaja ON Viesti.kayttaja_id = Kayttaja.id WHERE aihe_id = ?";
         List<Viesti> viestit = database.queryAndCollect(komento, new ViestiCollector(), aiheId);
@@ -21,5 +22,10 @@ public class ViestiDao {
         } else {
             return viestit;
         }
+    }
+
+    public void teeUusi(Integer kayttaja_id, Integer aihe_id, String viesti) throws SQLException {
+        String komento = "INSERT INTO Viesti (Kayttaja_id, Aihe_id, viesti) VALUES (?, ?, ?)";
+        database.update(komento, kayttaja_id, aihe_id, viesti);
     }
 }
