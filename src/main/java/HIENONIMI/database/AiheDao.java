7b0d2ad4250ea91419bc894296e71290e552cf74
hanninev <ville.hanninen94@gmail.com>
2016-10-13 10:56:18 +0300

    teeUusi ja etsiUusin-metodit (etsiUusin-metodin voisi tosin ehkä muokata siten, että järjestää timestampin perusteella)

--- src/main/java/HIENONIMI/database/AiheDao.java
+++ src/main/java/HIENONIMI/database/AiheDao.java
@@ -13,7 +13,6 @@ public class AiheDao {
     En implementannu Dao tähän, koska tätä käytetään vain yhdessä haussa ja Dao:n
     kaikki metodit ovat turhia. Ehkä..
      */
-
     public AiheDao(Database database) {
         this.database = database;
     }
@@ -28,6 +27,23 @@ public class AiheDao {
         } else {
             return aiheet;
         }
+    }
+
+    public Aihe etsiUusin(int alueId) throws SQLException { // Olisi varmaan parempi päivämäärän mukaan?
+        String komento = "SELECT Aihe.id AS id, Aihe.Alue_id, Aihe.nimi, count(Viesti.id) AS viesteja FROM Alue LEFT JOIN Aihe ON Alue.id = Aihe.Alue_id LEFT JOIN Viesti ON Aihe.id = Viesti.Aihe_id WHERE Aihe.alue_id = ? GROUP BY Aihe.id ORDER BY Aihe.id DESC";
+        List<Aihe> aiheet = database.queryAndCollect(komento, new AiheCollector(), alueId);
+
+        if (aiheet.isEmpty()) {
+            return null;
+        } else {
+            return aiheet.get(0);
+        }
+    }
+
+    public void teeUusi(Integer alue_id, String nimi) throws SQLException {
+        String komento = "INSERT INTO Aihe (Alue_id, nimi) VALUES (?, ?)";
+        System.out.println("tehtiin: " + nimi);
+        database.update(komento, alue_id, nimi);
     }
 
 }
